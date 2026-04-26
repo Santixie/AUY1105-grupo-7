3af268daca6540f291769c6546233bcea7952c12
Santixie <benj.santis@duocuc.cl>
2026-04-26 17:25:33 -0400

    Corregir advertencias de TFLint en main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.1.0.0/16"
@@ -20,18 +34,21 @@ resource "aws_subnet" "main" {
 # Security Group
 resource "aws_security_group" "main" {
   vpc_id = aws_vpc.main.id
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["10.1.0.0/16"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name        = "AUY1105-miapp-sg"
     Environment = "dev"
@@ -43,7 +60,9 @@ resource "aws_instance" "main" {
   ami           = var.ami_id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main.id
+
   vpc_security_group_ids = [aws_security_group.main.id]
+
   tags = {
     Name        = "AUY1105-miapp-ec2"
     Environment = "dev"
