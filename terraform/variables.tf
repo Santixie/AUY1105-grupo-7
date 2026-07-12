eb2d822bf48efc99ad4d3cc09040d41037c5bf1e
Santixie <benj.santis@duocuc.cl>
2026-07-12 12:24:33 -0400

    test: reintroducir valor invalido para confirmar que el gate ya arreglado bloquea

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,7 +37,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "Tipo de instancia EC2"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.small"
 }
 
 variable "existing_security_group_id" {
